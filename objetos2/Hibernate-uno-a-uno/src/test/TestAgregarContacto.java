1669f84f13e2f5781a4f5bddf65cfb18e7fb8980
PabloDL <pablo.delafore@hotmail.com>
2018-05-07 16:58:22 -0300

    Sigue fallando uno a muchos

--- objetos2/Hibernate-uno-a-uno/src/test/TestAgregarContacto.java
+++ objetos2/Hibernate-uno-a-uno/src/test/TestAgregarContacto.java
@@ -6,8 +6,9 @@ import datos.Cliente;
 import datos.Contacto;
 
 public class TestAgregarContacto {
-	public static void main(String [] args) {
-		
+
+	public static void main(String[] args) {
+
 		ClienteDao dao = new ClienteDao();
 		long idCliente = 0;
 		Cliente c = dao.traerCliente(idCliente);
@@ -15,5 +16,7 @@ public class TestAgregarContacto {
 		Contacto con = new Contacto("ajaramillo@unla.edu.ar" , "11-1111-1111", "011-1111-1111", c);
 		ContactoDao dao1 = new ContactoDao();
 		dao1.agregar(con);
+	
 	}
+
 }
